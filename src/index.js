1026fc3157268ac0369d2a11e3f07a7d35f63e9f
Lemuel Castro <lemuelbercasiocastro@gmail.com>
2021-02-24 13:40:39 +0800

    Replace typeface-roboto with @fontsource/roboto

--- src/index.js
+++ src/index.js
@@ -5,7 +5,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
-import 'typeface-roboto';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
